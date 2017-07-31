2f08cbd9d6bb0bd9c8eaa12dc1fd1b2b2e9b552e
jeetsrs <ranjeet@sodhi.org>
2017-07-31 18:33:22 -0400

    working basic server with nunjucks

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 const volleyball = require("volleyball");
+const nunjucks = require('nunjucks');
 
+const locals = {
+  title: 'An Example',
+  people: [
+    {name: 'Gandalf'},
+    {name: 'Frodo'},
+    {name: 'Hermione'}
+  ]
+}
+
+const people = [{name: 'Doris'}, {name: 'Ranjeet'}, {name: 'JS Code CRUSHERS!!'}];
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+
+
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
 
 app.listen(3000, function () {
   console.log('server listening');
@@ -21,6 +42,9 @@ app.get("/news", function (req, res) {
   res.send('Welcome to the news feed!');
 })
 
+
+//"index" is the file path that res.renders looks for (either an abs path or a path relative to the views folder)
 app.get("/", function (req, res) {
-  res.send('Welcome!');
+  // res.send('Welcome!');
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
 })
